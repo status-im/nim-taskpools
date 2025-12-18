938e759f42f926fa449b7d2d10991b9289fa76da
Jacek Sieka <jacek@status.im>
2025-12-18 12:01:24 +0100

    move comment

--- taskpools/taskpools.nim
+++ taskpools/taskpools.nim
@@ -464,11 +464,12 @@ macro spawn*(tp: Taskpool, fnCall: typed): untyped =
         # `move` to support move-only types in refc
         argsTup.add p
         let hasClosure = newLit(p.kind == nnkSym and hasClosure(p))
+
+        # `refc` uses a thread-local heap - therefore, anything heap-allocated
+        # cannot traverse thread boundaries, even if it's isolated - since
+        # tasks are likely to end up on a different thread, block their
+        # construction here.
         fwdCall.add quote do:
-          # `refc` uses a thread-local heap - therefore, anything heap-allocated
-          # cannot traverse thread boundaries, even if it's isolated - since
-          # tasks are likely to end up on a different thread, block their
-          # construction here.
           when (typeof(`p`) is (string|seq|ref)) or `hasClosure`:
             {.
               error:
